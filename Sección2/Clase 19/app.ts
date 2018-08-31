b078d5d4ba8d1a6b5476285c85b82d6ec2670c9d
Igor Gómez <gomez.chelseafc@gmail.com>
2018-08-31 14:58:00 -0700

    Sección 2 Clase 19 Interfaces en Typescript

---
+++ Sección2/Clase 19/app.ts
@@ -0,0 +1,20 @@
+interface Xmen{
+	nombre:string,
+	poder:string
+}
+
+function enviarMision(xmen : Xmen){
+	console.log("Enviando a: " + xmen.nombre);
+};
+
+function enviarCuartel(xmen : Xmen){
+	console.log("Enviando al cuartel: " + xmen.nombre);
+};
+
+let wolverine = {
+	nombre: "Wolverine",
+	poder: "Regeneración"
+}
+
+enviarMision(wolverine);
+enviarCuartel(wolverine);
