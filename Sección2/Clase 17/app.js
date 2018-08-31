5e2041abbbb5d9ca402012cb5c121f1d5d31d9ce
Igor Gómez <gomez.chelseafc@gmail.com>
2018-08-31 14:14:00 -0700

    Sección 2 Clase 17 Destructuración de Objetos

---
+++ Sección2/Clase 17/app.js
@@ -0,0 +1,10 @@
+var avenger = {
+    nombre: "Steve",
+    clave: "Capitan America",
+    poder: "Droga"
+};
+var poder = avenger.poder, nombre = avenger.nombre, clave = avenger.clave; //Destructuración de un objeto
+//let nombre = avenger.nombre;
+//let clave = avenger.clave;
+//let poder = avenger.poder;
+console.log(nombre, clave, poder);
---
+++ Sección2/Clase 17/app.ts
@@ -0,0 +1,14 @@
+let avenger = {
+	nombre: "Steve",
+	clave: "Capitan America",
+	poder: "Droga"
+}
+
+let {poder, nombre, clave} = avenger; //Destructuración de un objeto
+
+//let nombre = avenger.nombre;
+//let clave = avenger.clave;
+//let poder = avenger.poder;
+
+console.log(nombre, clave, poder);
+
