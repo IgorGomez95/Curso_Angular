fc4ed8a92fd98f62b6182f1da42d2cde29159102
Igor Gómez <gomez.chelseafc@gmail.com>
2018-08-31 22:14:55 -0700

    Sección 2 Clase 22 Clase y Constructor en Typescript

---
+++ Sección2/Clase 22/app.js
@@ -0,0 +1,12 @@
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.puedePelear = false;
+        this.peleasGanadas = 0;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antman = new Avenger("Antman", "Cap", "Scott Lang");
+console.log(antman);
